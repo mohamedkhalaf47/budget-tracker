1381ebbe30db078f1b532f5588a422a2d6eb1255
Mohamed M.Khalaf <mohamedmohamedkhalaf47@gmail.com>
2025-08-29 01:16:06 +0300

    Validation for Currency

---
+++ schema/userSettings.ts
@@ -0,0 +1,10 @@
+import { currencies } from "@/lib/currencies";
+import { z } from "zod";
+
+export const updateUserCurrencySchema = z.object({
+	currency: z.custom((value) => {
+		const found = currencies.some((c) => c.value === value);
+		if (!found) throw new Error(`Invalid currency ${value}`);
+		return value;
+	}),
+});
