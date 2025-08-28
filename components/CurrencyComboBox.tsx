050b9611c5513d8ce27118c2012830bc7903c98c
Mohamed M.Khalaf <mohamedmohamedkhalaf47@gmail.com>
2025-08-29 01:17:42 +0300

    Enhanced with user settings fetching and currency update functionality

--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -18,9 +18,12 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover";
-import { Currency,currencies } from "@/lib/currencies";
-
-
+import { Currency, currencies } from "@/lib/currencies";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "./SkeletonWrapper";
+import { UserSettings } from "@/lib/generated/prisma";
+import { UpdateUserCurrency } from "@/app/wizard/_actions/userSettings";
+import { toast } from "sonner";
 
 export function CurrencyComboBox() {
 	const [open, setOpen] = React.useState(false);
@@ -29,38 +32,104 @@ export function CurrencyComboBox() {
 		null
 	);
 
+	const userSettings = useQuery<UserSettings>({
+		queryKey: ["userSettings"],
+		queryFn: async () => {
+			const res = await fetch("/api/user-settings");
+			if (!res.ok) throw new Error("Failed To Fetch User Settings");
+			return res.json();
+		},
+	});
+
+	React.useEffect(() => {
+		if (!userSettings.data) return;
+		const userCurrency = currencies.find(
+			(currency) => currency.value === userSettings.data.currency
+		);
+		if (userCurrency) setSelectedOption(userCurrency);
+	}, [userSettings.data]);
+
+	const mutation = useMutation({
+		mutationFn: UpdateUserCurrency,
+		onSuccess: (data: UserSettings) => {
+			toast.success(`Currency Updated to ${data.currency}`, {
+				id: "update-currency",
+			});
+			setSelectedOption(
+				currencies.find((c) => c.value === data.currency) || null
+			);
+		},
+		onError: ()=>{
+			toast.error("Something Went Wrong",{
+				id: "update-currency"
+			})
+		}
+	});
+
+	const selectOption = React.useCallback(
+		(currency: Currency | null) => {
+			if (!currency) {
+				toast.error("Please Select a Currency");
+				return;
+			}
+
+			toast.loading("Updating Currency...", {
+				id: "update-currency",
+			});
+
+			mutation.mutate(currency.value);
+		},
+		[mutation]
+	);
+
 	if (isDesktop) {
 		return (
-			<Popover open={open} onOpenChange={setOpen}>
-				<PopoverTrigger asChild>
-					<Button variant="outline" className="w-fit justify-start">
+			<SkeletonWrapper isLoading={userSettings.isFetching}>
+				<Popover open={open} onOpenChange={setOpen}>
+					<PopoverTrigger asChild>
+						<Button
+							variant="outline"
+							className="w-fit justify-start"
+							disabled={mutation.isPending}
+						>
+							{selectedOption ? (
+								<>{selectedOption.label}</>
+							) : (
+								<>Choose a Currency</>
+							)}
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="w-[200px] p-0" align="start">
+						<OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+					</PopoverContent>
+				</Popover>
+			</SkeletonWrapper>
+		);
+	}
+
+	return (
+		<SkeletonWrapper isLoading={userSettings.isFetching}>
+			<Drawer open={open} onOpenChange={setOpen}>
+				<DrawerTrigger asChild>
+					<Button
+						variant="outline"
+						className="w-fit justify-start"
+						disabled={mutation.isPending}
+					>
 						{selectedOption ? (
 							<>{selectedOption.label}</>
 						) : (
 							<>Choose a Currency</>
 						)}
 					</Button>
-				</PopoverTrigger>
-				<PopoverContent className="w-[200px] p-0" align="start">
-					<OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-				</PopoverContent>
-			</Popover>
-		);
-	}
-
-	return (
-		<Drawer open={open} onOpenChange={setOpen}>
-			<DrawerTrigger asChild>
-				<Button variant="outline" className="w-fit justify-start">
-					{selectedOption ? <>{selectedOption.label}</> : <>Choose a Currency</>}
-				</Button>
-			</DrawerTrigger>
-			<DrawerContent>
-				<div className="mt-4 border-t">
-					<OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-				</div>
-			</DrawerContent>
-		</Drawer>
+				</DrawerTrigger>
+				<DrawerContent>
+					<div className="mt-4 border-t">
+						<OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+					</div>
+				</DrawerContent>
+			</Drawer>
+		</SkeletonWrapper>
 	);
 }
 
