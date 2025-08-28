cb8ed43fb73070c33a932cea865de31af8b0313f
Mohamed M.Khalaf <mohamedmohamedkhalaf47@gmail.com>
2025-08-29 01:13:11 +0300

    Changed To a Bank Icon

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,10 @@
-import { PiggyBank } from "lucide-react";
+import { Landmark } from "lucide-react";
 import Link from "next/link";
 
 export default function Logo(){
   return(
     <Link href="/" className="flex items-center gap-2">
-      <PiggyBank className="stroke w-11 h-11 stroke-amber-500 stroke-[1.5]"/>
+      <Landmark className="stroke w-11 h-11 stroke-amber-500 stroke-[1.5]"/>
       <p className="bg-gradient-to-r from-amber-400 to bg-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tight text-transparent">Budget Tracker</p>
     </Link>
   )
