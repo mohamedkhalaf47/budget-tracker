47a1815b09e9abdb08cebae6d341383e0c5d790a
Mohamed M.Khalaf <mohamedmohamedkhalaf47@gmail.com>
2025-09-13 15:50:57 +0300

    Finished GET history-data API Endpoint With All Functions Required

--- app/api/history-data/route.ts
+++ app/api/history-data/route.ts
@@ -32,6 +32,8 @@ export async function GET(req: Request) {
 		month: queryParams.data.month,
 		year: queryParams.data.year,
 	});
+
+	return Response.json(data);
 }
 
 export type GetHistoryDataResponseType = Awaited<
@@ -51,9 +53,100 @@ async function getHistoryData(
 	}
 }
 
-async function getYearHistoryData(userId: string, year: number) {}
+type HistoryData = {
+	expense: number;
+	income: number;
+	year: number;
+	month: number;
+	day?: number;
+};
+
+async function getYearHistoryData(userId: string, year: number) {
+	const result = await prisma.yearHistory.groupBy({
+		by: ["month"],
+		where: {
+			userId,
+			year,
+		},
+		_sum: {
+			expense: true,
+			income: true,
+		},
+		orderBy: [
+			{
+				month: "asc",
+			},
+		],
+	});
+
+	if (!result || result.length === 0) return [];
+	const history: HistoryData[] = [];
+
+	for (let i = 0; i < 12; i++) {
+		let expense = 0;
+		let income = 0;
+
+		const month = result.find((row) => row.month === i);
+		if (month) {
+			expense = month._sum.expense || 0;
+			income = month._sum.income || 0;
+		}
+
+		history.push({
+			month: i,
+			year,
+			expense,
+			income,
+		});
+	}
+
+	return history;
+}
 async function getMonthHistoryData(
 	userId: string,
 	year: number,
 	month: number
-) {}
+) {
+	const result = await prisma.monthHistory.groupBy({
+		by: ["day"],
+		where: {
+			userId,
+			year,
+			month,
+		},
+		_sum: {
+			expense: true,
+			income: true,
+		},
+		orderBy: [
+			{
+				day: "asc",
+			},
+		],
+	});
+
+	if (!result || result.length === 0) return [];
+
+	const history: HistoryData[] = [];
+
+	for (let i = 1; i <= 31; i++) {
+		let expense = 0;
+		let income = 0;
+
+		const day = result.find((row) => row.day === i);
+		if (day) {
+			expense = day._sum.expense || 0;
+			income = day._sum.income || 0;
+		}
+
+		history.push({
+			expense,
+			income,
+			month,
+			year,
+			day: i,
+		});
+	}
+
+	return history;
+}
